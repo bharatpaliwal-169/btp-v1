e0bac9a3b65c6eda4176f0725f3fc5a0021f64ad
Bharat Paliwal <18uec169@lnmiit.ac.in>
2021-03-22 11:48:48 +0530

    camera added ready for AI

--- js/main.js
+++ js/main.js
@@ -1,34 +1,43 @@
 const webcamElement = document.getElementById('webcam');
 const canvasElement = document.getElementById('canvas');
-// const snapSoundElement = document.getElementById('snapSound');
 const webcam = new Webcam(webcamElement, 'user', canvasElement);
-
 const stop = document.getElementById('stop');
+const start = document.getElementById('start');
+const snap = document.getElementById('snap');
 
 
 webcam.start()
-  .then(result =>{
-    console.log("webcam started");
-  })
-  .catch(err => {
-    console.log(err);
+    .then(result =>{
+      console.log("webcam started");
+    })
+    .catch(err => {
+      console.log(err);
+  });
+
+
+start.addEventListener('click', () => {
+  webcam.start();
+})
+
+
+var context = canvas.getContext('2d');
+snap.addEventListener('click', () => {
+  let picture = webcam.snap();
+  context.drawImage(picture, 0, 0);
+})
+
+stop.addEventListener('click', () => {
+  webcam.stop();
+})
+
+//mobile support
+$('#cameraFlip').click(function() {
+  webcam.flip();
+  webcam.start();  
 });
 
-let picture = webcam.snap();
-document.querySelector('#download-photo').href = picture;
-
-webcam.stop()
 
 
 
 
-// navigator.mediaDevices.enumerateDevices()
-// .then(getVideoInputs)
-// .catch(errorCallback);
-// function getVideoInputs(mediaDevices){
-// mediaDevices.forEach(mediaDevice => {
-//   if (mediaDevice.kind === 'videoinput') {
-//     this._webcamList.push(mediaDevice);
-//   }
-// });
-// }
+  
